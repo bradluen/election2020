cd58827b743ced52329909830248d4a19d9d8889
bradluen <bradluen@users.noreply.github.com>
2020-11-05 18:17:27 -0500

    Initial upload

---
+++ nytscraper2020.R
@@ -0,0 +1,34 @@
+scrapenyt = function(state, url){
+require(rvest)
+#url = "https://www.nytimes.com/interactive/2020/11/03/us/elections/results-indiana.html"
+webpage = read_html(url)
+tbls = html_nodes(webpage, "table")
+#this.table = tbls[grep("Monroe", tbls, ignore.case = TRUE)]
+this.table = tbls[2]
+df = html_table(this.table, fill = TRUE)[[1]]
+# Truncate last row
+df = df[-nrow(df),]
+# Remove extra "Est. votes reported" column
+df = df[, -5]
+# Remove blank rows
+df = filter(df, Margin != "—")
+# Change "Tied"
+df$Margin = recode(df$Margin, "Tied" = "Biden +0")
+# Get numeric margin
+results.list = strsplit(as.character(df$Margin), " ")
+results.list = matrix(unlist(results.list), byrow = T, ncol = 2)
+results.list[, 2] = gsub("<", "+", results.list[, 2])
+df$Winner2020 = as.factor(results.list[, 1])
+df$Margin2020 = as.numeric(results.list[, 2]) *
+  ifelse(df$Winner2020 == "Biden", 1, -1)
+# Get numeric voted reported
+df$counted = gsub("%", "", df$`Est. votes reported`)
+df$counted = gsub(">", "", df$counted)
+df$counted = as.numeric(df$counted)
+df$state = state
+# Make sure first column is called "County"
+names(df)[1] = "County"
+# Keep only necessary columns
+df = dplyr::select(df, County, Margin2020, counted, state)
+return(df)
+}
